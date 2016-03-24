47623b8fcee9b982e98b3e13f456b7a03049daf9
Tim Murray <timmurray@google.com>
2016-03-24 11:19:20 -0700

    mm: improve migration heuristic
    Some users were still seeing extreme unmovable page block migration over
    time due to unmovable allocations stealing mostly free movable
    blocks. Reduce the likelihood of this by only allowing unmovable
    allocations to aggressively steal reclaimable pageblocks.
    
    bug 26916944
    
    Change-Id: I87fe0b0963ea967e4edf1ef60ae3fd297bf6978c

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1168,11 +1168,15 @@ static bool can_steal_fallback(unsigned int order, int start_mt)
 	 */
 	if (order >= pageblock_order)
 		return true;
-
-	if (order >= pageblock_order / 2 ||
-		start_mt == MIGRATE_RECLAIMABLE ||
-		start_mt == MIGRATE_UNMOVABLE ||
-		page_group_by_mobility_disabled)
+	/* don't let unmovable allocations cause migrations simply because of free pages */
+	if ((start_mt != MIGRATE_UNMOVABLE && order >= pageblock_order / 2) ||
+	/* only steal reclaimable page blocks for unmovable allocations */
+	(start_mt == MIGRATE_UNMOVABLE && fallback_type != MIGRATE_MOVABLE && order >= pageblock_order / 2) ||
+	/* reclaimable can steal aggressively */
+	start_mt == MIGRATE_RECLAIMABLE ||
+	/* allow unmovable allocs up to 64K without migrating blocks */
+	(start_mt == MIGRATE_UNMOVABLE && start_order >= 5) ||
+	page_group_by_mobility_disabled)
 		return true;
 
 	return false;
